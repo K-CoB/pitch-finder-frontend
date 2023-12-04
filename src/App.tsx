f461d947de1cbf72d25099c23c417436c8914287
seondal <sseondal@gmail.com>
2023-12-05 01:09:31 +0900

    💄 style :  레이아웃 대충

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import "./App.css";
 import Audio from "./page/Audio";
 import Music from "./page/Music";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Pitch Finder</h1>
-        <Route path="/">
-          <Audio />
-        </Route>
-        <Route path="/music">
-          <Music />
-        </Route>
+      <div className="app">
+        <div className="view">
+          <div className="header">
+            <h1>Pitch Finder</h1>
+            <h5>
+              부르기 쉬운 노래를 찾고 싶은 당신을 위한 사용자 음높이 기반 가요
+              필터링 서비스
+            </h5>
+          </div>
+          <div className="body">
+            <Routes>
+              <Route path="/" element={<Audio />} />
+              <Route path="/music" element={<Music />} />
+            </Routes>
+          </div>
+        </div>
       </div>
     </BrowserRouter>
   );
