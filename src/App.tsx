7acff8db057b81e823767ad92eb23169ea074888
CSE-pebble <CSE.pebble@gmail.com>
2024-05-07 13:07:03 +0900

    [ing] ✨ feat : 헤더 컴포넌트 작업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import "./App.css";
-import Audio from "./page/Audio";
-import Music from "./page/Music";
+import Home from "./page/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <div className="view">
-          <div className="header">
-            <a href="/">
-              <h1>Pitch Finder</h1>
-            </a>
-            <h5>
-              부르기 쉬운 노래를 찾고 싶은 당신을 위한 사용자 음높이 기반 가요
-              필터링 서비스
-            </h5>
-            <h3 className="team">⭐️ 그로쓰 14팀 CoB ⭐️</h3>
-          </div>
-          <div className="body">
-            <Routes>
-              <Route path="/" element={<Audio />} />
-              <Route path="/music" element={<Music />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="">
+      <h1 className="text-black text-[40px]">PitchFinder</h1>
+      <h3>사용자 음높이 기반 가요 필터링 서비스</h3>
+    </div>
+  );
+}
