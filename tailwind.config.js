ec8a7cc2520cb24d356c2246869e9c94b5e02796
CSE-pebble <CSE.pebble@gmail.com>
2024-05-18 00:17:19 +0900

    💄style : TailwindCSS 전역 색상 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
         Ubuntu: ["Ubuntu", "sans-serif"],
       },
       colors: {
+        white: "#ffffff",
+        black: "#000000",
         "dark-gray": "#525252",
+        blue: {
+          base: "#6979F8", // 기본 버튼 색상
+          highlight: "#3647A7", // 클릭 시 버튼 색상
+          pitch: "#A1A1FF", // 음성 듣기 및 포기하기
+          "bar-1": "#D7D6FF", // 목표 음정 바
+          "bar-2": "#A1C1FF", // 현재 음정 바,결과 음역대 범위 바
+          result: "#E1E1FA",
+        },
       },
     },
   },
