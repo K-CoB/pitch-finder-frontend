c82c00aa7d3cc7f61afe76b08ae0bb3b2c002995
CSE-pebble <CSE.pebble@gmail.com>
2024-05-20 16:15:42 +0900

    ✨ feat : 성별 전역 상태 설정 및 성별에 따른 홈페이지 스타일 적용

--- src/components/home/MikeButton.tsx
+++ src/components/home/MikeButton.tsx
@@ -1,12 +1,32 @@
 import { ReactComponent as MicrophoneIcon } from "@/assets/microphone.svg";
+import { Link } from "react-router-dom";
 
-export default function MikeButton() {
-  return (
-    <div className="w-[251.62px] h-[249.23px] relative">
+interface MikeButtonProps {
+  isDisabled: boolean;
+}
+
+export default function MikeButton({ isDisabled }: MikeButtonProps) {
+  const buttonClass = isDisabled ? "opacity-50" : "cursor-pointer";
+
+  const buttonContent = (
+    <>
       <div className="w-[251.62px] h-[249.23px] left-0 top-0 absolute opacity-10 bg-indigo-300 rounded-full" />
       <div className="w-[221.43px] h-[219.32px] left-[15.10px] top-[14.95px] absolute opacity-40 bg-indigo-300 rounded-full" />
       <div className="w-[184.52px] h-[182.77px] left-[33.55px] top-[33.23px] absolute bg-purple-50 rounded-full shadow blur-[1px]" />
       <MicrophoneIcon className="w-[90px] h-[90px] left-[80.62px] top-[80.23px] absolute" />
+    </>
+  );
+
+  return isDisabled ? (
+    <div className={`w-[251.62px] h-[249.23px] relative ${buttonClass}`}>
+      {buttonContent}
     </div>
+  ) : (
+    <Link
+      to="/test"
+      className={`w-[251.62px] h-[249.23px] relative ${buttonClass}`}
+    >
+      {buttonContent}
+    </Link>
   );
 }
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,17 +1,41 @@
+import { useRecoilState } from "recoil";
+import { genderState } from "@/store/atom";
 import SelectButton from "@/components/common/SelectButton";
 import MikeButton from "@/components/home/MikeButton";
 
 export default function Home() {
+  const [gender, setGender] = useRecoilState(genderState);
+
+  const handleSelectGender = (selectedGender: string) => {
+    setGender((prevGender) =>
+      prevGender === selectedGender ? "" : selectedGender
+    );
+  };
+
   return (
     <div className="h-full items-center flex-column justify-evenly">
       <div className="flex-center flex-column gap-[15px]">
         <span className="text-black text-[14px]">성별을 선택해주세요.</span>
         <div className="flex gap-[25px]">
-          <SelectButton bgColor="bg-blue-base">남자</SelectButton>
-          <SelectButton bgColor="bg-blue-base">여자</SelectButton>
+          <SelectButton
+            onClick={() => handleSelectGender("남자")}
+            bgColor={`${
+              gender === "남자" ? "bg-blue-highlight" : "bg-blue-base"
+            }`}
+          >
+            남자
+          </SelectButton>
+          <SelectButton
+            onClick={() => handleSelectGender("여자")}
+            bgColor={`${
+              gender === "여자" ? "bg-blue-highlight" : "bg-blue-base"
+            }`}
+          >
+            여자
+          </SelectButton>
         </div>
       </div>
-      <MikeButton />
+      <MikeButton isDisabled={!gender} />
     </div>
   );
 }
