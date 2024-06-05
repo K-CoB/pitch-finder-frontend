6983b2a41d9a2ca78b8f8dde478aca81a4c74ac8
CSE-pebble <CSE.pebble@gmail.com>
2024-06-05 18:27:01 +0900

    ✂️ edit : 모든 bar에 음역 표시해줌에 따라 optional 삭제

--- src/components/result/ResultBar.tsx
+++ src/components/result/ResultBar.tsx
@@ -7,7 +7,7 @@ interface ResultBarProps {
   children: string;
   width?: number;
   marginLeft?: number;
-  average?: Average;
+  average: Average;
 }
 
 export default function ResultBar({
@@ -20,11 +20,9 @@ export default function ResultBar({
     <div className="flex-column gap-[11px]">
       <div className="flex gap-[8px]">
         <span>{children}</span>
-        {average && (
-          <span className="text-gray-400">
-            ({average?.low} ~ {average?.high})
-          </span>
-        )}
+        <span className="text-gray-400">
+          ({average.low} ~ {average.high})
+        </span>
       </div>
       <div className="w-full h-5 bg-blue-bg-bar rounded-[10px] flex-col justify-start items-start gap-2.5 inline-flex">
         <div
