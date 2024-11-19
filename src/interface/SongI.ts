ce164223a4936ec8fd205ea8f056bf28d50bb937
seondal <sseondal@gmail.com>
2024-11-19 20:06:02 +0900

    ✨ feat :  백엔드 종료..

--- src/interface/SongI.ts
+++ src/interface/SongI.ts
@@ -1,3 +1,15 @@
+export interface SongDataI {
+  id: number;
+  singer: string;
+  song: string;
+  highest_pitch: number;
+  lowest_pitch: number;
+  gender: number;
+  youtube_url: string;
+  youtube_image: string;
+  youtube_listen_url: string;
+}
+
 export interface SongI {
   singer: string;
   song: string;
--- src/page/Music.tsx
+++ src/page/Music.tsx
@@ -6,6 +6,7 @@ import { SongI } from "@/interface/SongI";
 import axios, { AxiosResponse } from "axios";
 import ENV from "@/constants/env";
 import VALUE from "@/constants/value";
+import MUSIC_DATA from "@/data/MUSIC_DATA";
 
 // TODO: 빈 화면 분기처리 필요
 // TODO: 정렬 순서 처리 필요
@@ -20,14 +21,21 @@ export default function Music() {
   const [musicList, setMusicList] = useState<SongI[]>(Songs);
 
   useEffect(() => {
-    axios
-      .get<SongI[]>(ENV.api, {
-        params: { high: highLimit, low: lowLimit },
-        headers: { "x-api-key": ENV.api_key },
-      })
-      .then((response: AxiosResponse<SongI[]>) => {
-        setMusicList(response.data);
-      });
+    const data = MUSIC_DATA.filter(
+      (item) => item.highest_pitch <= highLimit && item.lowest_pitch >= lowLimit
+    );
+    const result: SongI[] = data.map((item) => ({
+      singer: item.singer,
+      song: item.song,
+      high: item.highest_pitch,
+      low: item.lowest_pitch,
+      image: item.youtube_image,
+      url: {
+        listen: item.youtube_listen_url,
+        sing: item.youtube_url,
+      },
+    }));
+    setMusicList(result);
   }, [highLimit, lowLimit]);
 
   // TODO : url 복사 기능 구현 필요
@@ -102,43 +110,47 @@ export default function Music() {
         </div>
         <span>키 더 낮게 한다면</span>
       </div>
-      <div>
-        {musicList.map((music, idx) => (
-          <li
-            key={idx}
-            className="flex justify-between mb-4 bg-white p-2 rounded-lg">
-            <section className="flex gap-2">
-              <img src={music.image} className="w-12 h-12 rounded-md" />
-              <div className="flex flex-col">
-                <span>{music.song}</span>
-                <span className="text-sm text-slate-500">{music.singer}</span>
-              </div>
-            </section>
-            <section className="flex">
-              <ul className="flex items-center gap-2">
-                <li className="bg-[#FFAE35] text-white rounded-lg px-2 text-sm">
-                  {getPitchFromNote(music.high).pitch}
-                </li>
-                <li className="bg-[#4535FF] text-white rounded-lg px-2 text-sm">
-                  {getPitchFromNote(music.low).pitch}
-                </li>
-              </ul>
-              <ul className="ml-4 text-lg flex items-center gap-2">
-                <a
-                  className="bg-slate-200 rounded-full p-2"
-                  href={music.url.listen}>
-                  🎧
-                </a>
-                <a
-                  className="bg-slate-200 rounded-full p-2"
-                  href={music.url.sing}>
-                  🎤
-                </a>
-              </ul>
-            </section>
-          </li>
-        ))}
-      </div>
+      {musicList.length === 0 ? (
+        <div>앗! 데이터가 없어요</div>
+      ) : (
+        <div>
+          {musicList.map((music, idx) => (
+            <li
+              key={idx}
+              className="flex justify-between mb-4 bg-white p-2 rounded-lg">
+              <section className="flex gap-2">
+                <img src={music.image} className="w-12 h-12 rounded-md" />
+                <div className="flex flex-col">
+                  <span>{music.song}</span>
+                  <span className="text-sm text-slate-500">{music.singer}</span>
+                </div>
+              </section>
+              <section className="flex">
+                <ul className="flex items-center gap-2">
+                  <li className="bg-[#FFAE35] text-white rounded-lg px-2 text-sm">
+                    {getPitchFromNote(music.high).pitch}
+                  </li>
+                  <li className="bg-[#4535FF] text-white rounded-lg px-2 text-sm">
+                    {getPitchFromNote(music.low).pitch}
+                  </li>
+                </ul>
+                <ul className="ml-4 text-lg flex items-center gap-2">
+                  <a
+                    className="bg-slate-200 rounded-full p-2"
+                    href={music.url.listen}>
+                    🎧
+                  </a>
+                  <a
+                    className="bg-slate-200 rounded-full p-2"
+                    href={music.url.sing}>
+                    🎤
+                  </a>
+                </ul>
+              </section>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
